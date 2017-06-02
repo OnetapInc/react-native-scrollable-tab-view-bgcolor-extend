b10f15229ed0e96e280eadc82294dfa65755ae8f
goleador0322 <goleador0322@gmail.com>
2017-06-02 18:08:31 +0900

    bug fix and update

--- ScrollableTabBar.js
+++ ScrollableTabBar.js
@@ -41,6 +41,7 @@ const ScrollableTabBar = React.createClass({
       backgroundColor: null,
       style: {},
       tabStyle: {},
+      activeTabStyle: {},
       tabsContainerStyle: {},
       underlineStyle: {},
     };
